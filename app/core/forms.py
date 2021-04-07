da259b6b2144c73481d203039e5a31667c8ecac7
conjectures <alexis.othonos@gmail.com>
2021-04-07 17:55:04 +0100

    fixed datetime picker, removed crispy form dependency, added others

--- app/core/forms.py
+++ app/core/forms.py
@@ -3,7 +3,8 @@ from django.forms.models import ModelForm
 from crispy_forms.bootstrap import Field, InlineRadios, TabHolder, Tab
 from crispy_forms.helper import FormHelper
 
-from crispy_forms.layout import Submit, Layout, Div, Fieldset
+# from crispy_forms.layout import Submit, Layout, Div, Fieldset
+from bootstrap_datepicker_plus import DateTimePickerInput, TimePickerInput
 
 from .models import Flight, Aircraft, Passenger
 
@@ -16,8 +17,8 @@ class FlightsForm(forms.ModelForm):
         widgets= {
                 'origin': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Stansted'}),
                 'destination': forms.TextInput(attrs={'class': 'form-control'}),
-                'departure_time': forms.DateTimeInput(attrs={'class': 'form-control'}),
-                'duration': forms.TimeInput(attrs={'class': 'form-control'}),
+                'departure_time': DateTimePickerInput(),
+                'duration': TimePickerInput(attrs={'class': 'form-control'}),
                 'aircraft_id': forms.Select(attrs={'class': 'form-control'}),
                 'attendance': forms.SelectMultiple(attrs={'class': 'form-control',}),
                 }
